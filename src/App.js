b5f5376c6957999a9621c17216f0936abc92cbb5
JA <joshappeldev@gmail.com>
2019-07-03 13:26:22 -0400

    App as functional component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Landing from './components/Landing';
@@ -14,24 +14,20 @@ import { withAuthentication } from './components/Session/index';
 import * as ROUTES from './constants/routes';
 import './App.scss';
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <Navigation />
-        <Route path={ROUTES.HOME} component={Home} />
-        <Route path={ROUTES.DMS} component={DirectMessages} />
-        <Container>
-          <Route exact path={ROUTES.LANDING} component={Landing} />
-          <Route path={ROUTES.SIGN_UP} component={SignUp} />
-          <Route path={ROUTES.SIGN_IN} component={SignIn} />
-          <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForget} />
-          <Route path={ROUTES.ACCOUNT} component={Account} />
-          <Route path={ROUTES.ADMIN} component={Admin} />
-        </Container>
-      </Router>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <Navigation />
+    <Route path={ROUTES.HOME} component={Home} />
+    <Route path={ROUTES.DMS} component={DirectMessages} />
+    <Container>
+      <Route exact path={ROUTES.LANDING} component={Landing} />
+      <Route path={ROUTES.SIGN_UP} component={SignUp} />
+      <Route path={ROUTES.SIGN_IN} component={SignIn} />
+      <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForget} />
+      <Route path={ROUTES.ACCOUNT} component={Account} />
+      <Route path={ROUTES.ADMIN} component={Admin} />
+    </Container>
+  </Router>
+);
 
 export default withAuthentication(App);

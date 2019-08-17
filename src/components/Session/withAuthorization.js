a274e1159c14a3e3bbeb7fc164e0c7ea71febcc8
jhadev <joshappeldev@gmail.com>
2019-08-17 09:41:01 -0400

    refactored classes into functions

--- src/components/Session/withAuthorization.js
+++ src/components/Session/withAuthorization.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 import { compose } from 'recompose';
 import AuthUserContext from './context';
@@ -6,34 +6,27 @@ import { withFirebase } from '../Firebase/index';
 import * as ROUTES from '../../constants/routes';
 
 const withAuthorization = condition => Component => {
-  class WithAuthorization extends React.Component {
-    componentDidMount() {
-      //use firebase listener to execute when an authed user changes
-      this.listener = this.props.firebase.auth.onAuthStateChanged(authUser => {
+  const WithAuthorization = props => {
+    useEffect(() => {
+      const listener = props.firebase.auth.onAuthStateChanged(authUser => {
         //if auth fails send the user to the signin page
         if (!condition(authUser)) {
           //with history object of from react router
-          this.props.history.push(ROUTES.SIGN_IN);
+          props.history.push(ROUTES.SIGN_IN);
         }
       });
-    }
-    //avoid memory leaks
-    componentWillUnmount() {
-      this.listener();
-    }
+      return () => listener();
+    }, [props.firebase.auth, props.history]);
 
-    render() {
-      return (
-        //consume auth user from the context
-        //avoid showing protected page before the redirect using the higher order component
-        <AuthUserContext.Consumer>
-          {authUser =>
-            condition(authUser) ? <Component {...this.props} /> : null
-          }
-        </AuthUserContext.Consumer>
-      );
-    }
-  }
+    return (
+      //consume auth user from the context
+      //avoid showing protected page before the redirect using the higher order component
+      <AuthUserContext.Consumer>
+        {authUser => (condition(authUser) ? <Component {...props} /> : null)}
+      </AuthUserContext.Consumer>
+    );
+  };
+
   return compose(
     withRouter,
     withFirebase

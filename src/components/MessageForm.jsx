c0ed06d03b0071888b1f3e926de96245f85a3f97
jhadev <joshappeldev@gmail.com>
2019-09-16 21:34:15 -0400

    handle multiple typers, if user signs out still typing reflect that in db

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -160,26 +160,23 @@ const MessageForm = ({
   return (
     <>
       <div className="sticky-footer">
-        {whoseTyping.length > 0 &&
-          (whoseTyping.length === 1 ? (
-            <div className="typers">
+        {whoseTyping.length > 0 && (
+          <div className="typers">
+            {whoseTyping.length === 1 ? (
               <span className="focus-in-expand-fwd">
                 {whoseTyping.join(', ')} is typing...
               </span>
-            </div>
-          ) : whoseTyping.length > 1 && whoseTyping.length <= 3 ? (
-            <div className="typers">
+            ) : whoseTyping.length > 1 && whoseTyping.length <= 3 ? (
               <span className="focus-in-expand-fwd">
                 {whoseTyping.join(', ')} are typing...
               </span>
-            </div>
-          ) : (
-            <div className="typers">
+            ) : (
               <span className="focus-in-expand-fwd">
                 multiple users are typing...
               </span>
-            </div>
-          ))}
+            )}
+          </div>
+        )}
         <Form onSubmit={sendNewMessage}>
           <FormGroup id="messageForm" row>
             <Label
